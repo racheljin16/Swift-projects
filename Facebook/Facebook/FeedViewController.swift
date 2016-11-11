01624d22ed39e08d12c3a52d3e5e076cdd9ac492
rachel <rachel@Rachels-MacBook-Pro.local>
2016-11-10 21:14:48 -0800

    Finish basic lighbox animation

--- Facebook/Facebook/FeedViewController.swift
+++ Facebook/Facebook/FeedViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class FeedViewController: UIViewController {
+    
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIView!
+    var lightBoxTransition: LightBoxTransition!
+    var fadeTransition: FadeTransition!
+    var selectedImageView: UIImageView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.contentSize = imageView.frame.size
+        lightBoxTransition = LightBoxTransition()
+        fadeTransition = FadeTransition()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +38,21 @@ class FeedViewController: UIViewController {
         scrollView.scrollIndicatorInsets.top = 0
         scrollView.scrollIndicatorInsets.bottom = 50
         
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let destinationVC = segue.destination as! PhotoViewController
+        destinationVC.image = self.selectedImageView.image
+        destinationVC.modalPresentationStyle = .custom
+        destinationVC.transitioningDelegate = self.lightBoxTransition
+        lightBoxTransition.duration = 1
+    }
+        
+
+    @IBAction func onTapPhoto(_ sender: UITapGestureRecognizer) {
+        selectedImageView = sender.view as! UIImageView
+        performSegue(withIdentifier: "toPhotoSegue", sender: nil)
     }
 
 }
